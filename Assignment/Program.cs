71c55f643820ea3eca134b0f42f1bdc488fe787e
WissamTarik <www.wissamtarik@gmail.com>
2025-07-15 20:31:03 +0300

    Q1 Part02

---
+++ Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Assignment
+{
+    #region Part02
+    #region Q1
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+    #endregion
+
+
+
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part02
+            #region Q1
+            /*
+             
+            1-	Create an enum called "WeekDays" with the days of the week (Monday to Sunday)
+            as its members. Then, write a C# program that prints
+             out all the days of the week using this enum.
+
+             */
+
+            //foreach (string item in Enum.GetNames(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+
+            #endregion
+        }
+    }
+}
